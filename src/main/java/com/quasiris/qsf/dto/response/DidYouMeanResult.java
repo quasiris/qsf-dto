f6a9ef31f5eca92a3a42454aa85c8916b098e1bc
Thomas Bärwinkel <tbl@gmx.de>
2021-11-05 12:22:37 +0100

    enhance did you bean object for improved spell correction

--- src/main/java/com/quasiris/qsf/dto/response/DidYouMeanResult.java
+++ src/main/java/com/quasiris/qsf/dto/response/DidYouMeanResult.java
@@ -7,7 +7,14 @@ public class DidYouMeanResult {
 
     private String name;
 
+    // didYouMean, corrected
+    private String type;
+
     private String didYouMean;
+
+    private String corrected;
+
+    private String original;
 
     public String getName() {
         return name;
@@ -23,5 +30,59 @@ public class DidYouMeanResult {
 
     public void setDidYouMean(String didYouMean) {
         this.didYouMean = didYouMean;
+    }
+
+    /**
+     * Getter for property 'type'.
+     *
+     * @return Value for property 'type'.
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * Setter for property 'type'.
+     *
+     * @param type Value to set for property 'type'.
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    /**
+     * Getter for property 'corrected'.
+     *
+     * @return Value for property 'corrected'.
+     */
+    public String getCorrected() {
+        return corrected;
+    }
+
+    /**
+     * Setter for property 'corrected'.
+     *
+     * @param corrected Value to set for property 'corrected'.
+     */
+    public void setCorrected(String corrected) {
+        this.corrected = corrected;
+    }
+
+    /**
+     * Getter for property 'original'.
+     *
+     * @return Value for property 'original'.
+     */
+    public String getOriginal() {
+        return original;
+    }
+
+    /**
+     * Setter for property 'original'.
+     *
+     * @param original Value to set for property 'original'.
+     */
+    public void setOriginal(String original) {
+        this.original = original;
     }
 }
