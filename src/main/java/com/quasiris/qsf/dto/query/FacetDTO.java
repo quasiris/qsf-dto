0e90fef79e14ac4efa6cc6dfb59c02dcc87a6b23
Thomas Bärwinkel <tbl@gmx.de>
2021-11-11 19:36:38 +0100

    add getter and setter for facet result config

--- src/main/java/com/quasiris/qsf/dto/query/FacetDTO.java
+++ src/main/java/com/quasiris/qsf/dto/query/FacetDTO.java
@@ -13,5 +13,39 @@ public class FacetDTO {
 
     private List<FacetsDTO> facets;
 
+    /**
+     * Getter for property 'enabled'.
+     *
+     * @return Value for property 'enabled'.
+     */
+    public Boolean getEnabled() {
+        return enabled;
+    }
 
+    /**
+     * Setter for property 'enabled'.
+     *
+     * @param enabled Value to set for property 'enabled'.
+     */
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    /**
+     * Getter for property 'facets'.
+     *
+     * @return Value for property 'facets'.
+     */
+    public List<FacetsDTO> getFacets() {
+        return facets;
+    }
+
+    /**
+     * Setter for property 'facets'.
+     *
+     * @param facets Value to set for property 'facets'.
+     */
+    public void setFacets(List<FacetsDTO> facets) {
+        this.facets = facets;
+    }
 }
