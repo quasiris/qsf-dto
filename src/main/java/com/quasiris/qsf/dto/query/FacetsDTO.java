e8b50bce9c6761088e2867911c9e22a288e25a79
Thomas Bärwinkel <tbl@quasiris.de>
2022-05-11 23:07:32 +0200

    make facets configurable for requests

--- src/main/java/com/quasiris/qsf/dto/query/FacetsDTO.java
+++ src/main/java/com/quasiris/qsf/dto/query/FacetsDTO.java
@@ -10,4 +10,32 @@ import java.util.List;
 public class FacetsDTO {
 
 
+    private String id;
+
+    private Integer displayCount;
+    private String sort;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Integer getDisplayCount() {
+        return displayCount;
+    }
+
+    public void setDisplayCount(Integer displayCount) {
+        this.displayCount = displayCount;
+    }
+
+    public String getSort() {
+        return sort;
+    }
+
+    public void setSort(String sort) {
+        this.sort = sort;
+    }
 }
