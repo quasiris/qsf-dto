95072afa07b42ba96e8bd80e9f768baae5e8c8f8
Jan Bajaryn <jbn@quasiris.de>
2024-01-19 09:32:14 +0100

    QSC-1613 fix duplicated property 'opType' in BaseSearchFilterDTO

--- src/main/java/com/quasiris/qsf/dto/query/BaseSearchFilterDTO.java
+++ src/main/java/com/quasiris/qsf/dto/query/BaseSearchFilterDTO.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.quasiris.qsf.dto.common.BaseDTO;
 
-@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "opType", defaultImpl = SearchFilterDTO.class)
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.EXISTING_PROPERTY, property = "opType", defaultImpl = SearchFilterDTO.class)
 @JsonSubTypes({
         @JsonSubTypes.Type(value = SearchFilterDTO.class, name = "filter"),
         @JsonSubTypes.Type(value = BoolSearchFilterDTO.class, name = "bool")
