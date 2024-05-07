30f301cd11282330d7361def91c85267d706480e
Thomas Bärwinkel <thomas.baerwinkel@quasiris.de>
2024-05-07 11:24:29 +0200

    add HistogramFacetConfigDTO

--- src/main/java/com/quasiris/qsf/dto/query/HistogramFacetConfigDTO.java
+++ src/main/java/com/quasiris/qsf/dto/query/HistogramFacetConfigDTO.java
@@ -1,8 +1,16 @@
 package com.quasiris.qsf.dto.query;
 
+import com.fasterxml.jackson.databind.JsonNode;
+
 import java.util.List;
 
 public class HistogramFacetConfigDTO {
+
+    private String timeZone;
+
+    private Integer minDocCount;
+
+    private JsonNode query;
 
     private List<IntervalDTO> intervals;
 
@@ -12,5 +20,29 @@ public class HistogramFacetConfigDTO {
 
     public void setIntervals(List<IntervalDTO> intervals) {
         this.intervals = intervals;
+    }
+
+    public String getTimeZone() {
+        return timeZone;
+    }
+
+    public void setTimeZone(String timeZone) {
+        this.timeZone = timeZone;
+    }
+
+    public Integer getMinDocCount() {
+        return minDocCount;
+    }
+
+    public void setMinDocCount(Integer minDocCount) {
+        this.minDocCount = minDocCount;
+    }
+
+    public JsonNode getQuery() {
+        return query;
+    }
+
+    public void setQuery(JsonNode query) {
+        this.query = query;
     }
 }
